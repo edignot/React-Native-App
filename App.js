b323b8c429def4c27fc4dda479afe8a7027b5b5b
Ed Ignot <edignot@gmail.com>
2020-08-28 20:32:44 -0600

    Add list items

--- App.js
+++ App.js
@@ -1,19 +1,47 @@
 import { StatusBar } from 'expo-status-bar'
 import React, { useState } from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Change this text')
+  const [enteredGoal, setEnteredGoal] = useState('')
+  const [goals, setGoals] = useState([])
 
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText)
+  }
+
+  const addGoalHandler = () => {
+    setGoals((currentGoals) => [...currentGoals, enteredGoal])
+  }
   return (
-    // VIEW IS A CONTAINER COMPONENT (like div or section )
-    // CANNOT PUT TEXT IN A VIEW WITHOUT TEXT WRAPPER
-    <View>
-      <View></View>
-      <View></View>
-      <Text>{outputText}</Text>
-      <StatusBar style='error' />
-      <Button title='Hi' onPress={() => setOutputText('Changed')} />
+    <View style={{ padding: 50 }}>
+      <View style={styles.screen}>
+        <TextInput
+          placeholder='Goal'
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title='Add' style={{ flex: 2 }} onPress={addGoalHandler} />
+      </View>
+      <View>
+        {goals.map((goal) => (
+          <Text>{goal}</Text>
+        ))}
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'grey',
+    flex: 1,
+  },
+})
