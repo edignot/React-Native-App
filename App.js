1376a0969da39cc3cca6059058759066fdf69c01
Ed Ignot <edignot@gmail.com>
2020-08-28 19:37:48 -0600

    Initial setup

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [outputText, setOutputText] = useState('Change this text')
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    // VIEW IS A CONTAINER COMPONENT (like div or section )
+    // CANNOT PUT TEXT IN A VIEW WITHOUT TEXT WRAPPER
+    <View>
+      <View></View>
+      <View></View>
+      <Text>{outputText}</Text>
+      <StatusBar style='error' />
+      <Button title='Hi' onPress={() => setOutputText('Changed')} />
+    </View>
+  )
+}
